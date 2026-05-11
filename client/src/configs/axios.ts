ccd3a503365f12973e18c46e2b07839d84a30624
Ayush Raj <kamalayush65@gmail.com>
2026-05-11 14:44:35 +0530

    axios

--- client/src/configs/axios.ts
+++ client/src/configs/axios.ts
@@ -1,16 +1,10 @@
-
-import axios from 'axios';
-
+import axios from 'axios'
 
 const api = axios.create({
-<<<<<<< HEAD
-    baseURL: import.meta.env.VITE_BASEURL || 'https://ai-site-builder-zeta.vercel.app',
-=======
-    baseURL: import.meta.env.VITE_BASEURL || 'https://ai-site-builder-saua.onrender.com/',
->>>>>>> a718e5a (final backend fix)
-    withCredentials: true
-
-
+  baseURL:
+    import.meta.env.VITE_BASEURL ||
+    'https://ai-site-builder-saua.onrender.com',
+  withCredentials: true,
 })
 
 export default api
