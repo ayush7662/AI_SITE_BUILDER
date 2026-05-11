06f01555f98b9e18e838a0fb509421716173b8a4
AYUSH RAJ <127328301+ayush7662@users.noreply.github.com>
2026-05-11 14:32:41 +0530

    Update axios.ts

--- client/src/configs/axios.ts
+++ client/src/configs/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_BASEURL || 'http://localhost:3000',
+    baseURL: import.meta.env.VITE_BASEURL || 'https://ai-site-builder-zeta.vercel.app',
     withCredentials: true
 
 
