b2c402dc260fc5c9e73805096e01ab8413ebda32
AYUSH RAJ <127328301+ayush7662@users.noreply.github.com>
2026-05-12 08:53:18 +0530

    Update axios.ts

--- client/src/configs/axios.ts
+++ client/src/configs/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 const api = axios.create({
   baseURL:
     import.meta.env.VITE_BASEURL ||
-    'http://localhost:3000',
+    'https://ai-site-builder-saua.onrender.com',
   withCredentials: true,
 })
 
